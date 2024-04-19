d687a671e6f203dfa628c1a3f527244161cfec62
rinoa349 <chan2s@icloud.com>
2024-04-19 16:47:53 +0900

    check.php追加

---
+++ join/check.php
@@ -0,0 +1,41 @@
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>会員登録</title>
+
+  <link rel="stylesheet" href="../style.css"/>
+</head>
+<body>
+  <div id="wrap">
+    <div id="head">
+      <h1>会員登録</h1>
+    </div>
+
+    <div id="content">
+      <p>記入内容を確認のうえ、「登録する」ボタンをクリックしてください。</p>
+      <form action="" method="post">
+        <dl>
+          <dt>ニックネーム</dt>
+          <dd></dd>
+          <dt>メールアドレス</dt>
+          <dd></dd>
+          <dt>パスワード</dt>
+          <dd>
+            【表示されません】
+          </dd>
+          <dt>画像/ファイルのアップロード</dt>
+          <dd>
+            <img src="../member_picture/" width="100" alr="" />
+          </dd>
+        </dl>
+        <div><a href="index.php?action=rewrite">書き直す</a> | <input type="submit" value="登録する" /></div>
+      </form>
+    </div>
+  </div>
+  
+</body>
+</html>
